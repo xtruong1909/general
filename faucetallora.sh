1967a745001e6f8e1a5d65c46e30850c459ebf97
hiepntnaa <160826386+hiepntnaa@users.noreply.github.com>
2024-07-10 20:43:56 +0300

    Update faucetallora.sh

--- faucetallora.sh
+++ faucetallora.sh
@@ -4,10 +4,9 @@ cd allora-chain/
 
 echo "$Pass" | allorad keys list
 
-# Check address
-if [ -z "$address" ]; then
-  read -p "Paste address here: " address
-fi
+read -p "Paste address here: " address
+echo $address
+
 
 for ((i=1; i<=50; i++)); do
   echo "faucet n $i..."
