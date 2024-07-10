68876e838cdc38e54be9163383c6272eb7d48d58
hiepntnaa <160826386+hiepntnaa@users.noreply.github.com>
2024-07-10 20:14:13 +0300

    Create faucetallora.sh

---
+++ faucetallora.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd allora-chain
+
+echo "Pass" | allorad keys list
+
+read -p "paste address here: " address
+
+for ((i=1; i<=20; i++)); do
+  echo "faucet n $i..."
+  curl -sS https://faucet.edgenet.allora.network/send/edgenet/$address
+  sleep 5
+done
+
+echo "Finish"
