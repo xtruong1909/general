d95fe88f7a701361e35f2dca7dd50afa78ed9110
hiepntnaa <160826386+hiepntnaa@users.noreply.github.com>
2024-07-10 08:40:03 +0300

    Update limitcpu.sh

--- limitcpu.sh
+++ limitcpu.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-cpu_limit=90
+apt install cgroup-tools
+sudo cgcreate -g cpu:cpulimit_group
+sudo mkdir -p /sys/fs/cgroup/cpu
+sudo bash -c 'echo "80000" > /sys/fs/cgroup/cpu/cpu.max'
+sudo cgexec -g cpu:cpulimit_group systemctl restart nubit
 
-while true; do
-  for pid in $(ps -e -o pid=); do
-    # Kiem tra neu cpulimit da duoc ap dung cho tien trinh nay hay chua
-    if ! ps -p $pid -o cmd= | grep -q "cpulimit"; then
-      # Ap dung cpulimit cho tien trinh
-      sudo cpulimit -l $cpu_limit -p $pid -b
-    fi
-  done
-  sleep 5
+
+# Names of Docker containers to limit CPU
+containers=("inference-basic-eth-pred" "updater-basic-eth-pred" "worker-basic-eth-pred" "head-basic-eth-pred")
+
+# Create a new cgroup directory if it doesn't exist
+sudo mkdir -p /sys/fs/cgroup/cpu/docker_limit_group
+
+# Set CPU limit for each container
+for container in "${containers[@]}"
+do
+    # Get the PID of the container
+    container_pid=$(docker inspect -f '{{.State.Pid}}' "$container")
+
+    # Set up cgroup for the container
+    sudo mkdir -p /sys/fs/cgroup/cpu/docker_limit_group/$container_pid
+    echo $container_pid > /sys/fs/cgroup/cpu/docker_limit_group/$container_pid/tasks
+
+    # Set CPU limit for the cgroup
+    echo 80000 > /sys/fs/cgroup/cpu/docker_limit_group/cpu.cfs_quota_us
+    echo 100000 > /sys/fs/cgroup/cpu/docker_limit_group/cpu.cfs_period_us
+
+    echo "Container $container CPU limited."
 done
