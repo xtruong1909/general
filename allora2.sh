db450d0fd717e475a71579ff69269f404e488299
hiepntnaa <160826386+hiepntnaa@users.noreply.github.com>
2024-07-15 21:31:25 +0700

    Create allora2.sh

---
+++ allora2.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+
+execute_with_prompt() {
+    echo "Executing: $1"
+    if eval "$1"; then
+        echo "Command executed successfully."
+    else
+        echo "Error executing command: $1"
+        exit 1
+    fi
+}
+
+cd allora-chain/basic-coin-prediction-node-2
+echo "Creating Head keys..."
+echo
+sudo docker run -it --entrypoint=bash -v $(pwd)/head-data:/data alloranetwork/allora-inference-base:latest -c "mkdir -p /data/keys && (cd /data/keys && allora-keys)"
+echo
+sudo docker run -it --entrypoint=bash -v $(pwd)/worker-data:/data alloranetwork/allora-inference-base:latest -c "mkdir -p /data/keys && (cd /data/keys && allora-keys)"
+echo
+
+echo "This is your Head ID:"
+HEAD_ID=$(cat head-data/keys/identity)
+echo "$HEAD_ID"
+echo
+
+if [ -f docker-compose.yml ]; then
+    rm docker-compose.yml
+    echo "Removed existing docker-compose.yml file."
+    echo
+fi
+
+read -p "Enter WALLET_SEED_PHRASE: " WALLET_SEED_PHRASE
+echo
+
+echo "Generating docker-compose.yml file..."
+cat <<EOF > docker-compose.yml
+version: '3'
+services:
+  inference:
+    container_name: inference-basic-eth-pred-2
+    build:
+      context: .
+    command: python -u /app/app.py
+    ports:
+      - "8010:8010"
+    networks:
+      eth-model-local:
+        aliases:
+          - inference
+        ipv4_address: 172.22.0.4
+    healthcheck:
+      test: ["CMD", "curl", "-f", "http://localhost:8010/inference/ETH"]
+      interval: 10s
+      timeout: 10s
+      retries: 12
+    volumes:
+      - ./inference-data:/app/data
+    restart: always
+
+  updater:
+    container_name: updater-basic-eth-pred-2
+    build: .
+    environment:
+      - INFERENCE_API_ADDRESS=http://inference:8010
+    command: >
+      sh -c "
+      while true; do
+        python -u /app/update_app.py;
+        sleep 24h;
+      done
+      "
+    depends_on:
+      inference:
+        condition: service_healthy
+    networks:
+      eth-model-local:
+        aliases:
+          - updater
+        ipv4_address: 172.22.0.5
+    restart: always
+
+  worker:
+    container_name: worker-basic-eth-pred-2
+    environment:
+      - INFERENCE_API_ADDRESS=http://inference:8010
+      - HOME=/data
+    build:
+      context: .
+      dockerfile: Dockerfile_b7s
+    entrypoint:
+      - "/bin/bash"
+      - "-c"
+      - |
+        if [ ! -f /data/keys/priv.bin ]; then
+          echo "Generating new private keys..."
+          mkdir -p /data/keys
+          cd /data/keys
+          allora-keys
+        fi
+        allora-node --role=worker --peer-db=/data/peerdb --function-db=/data/function-db \
+          --runtime-path=/app/runtime --runtime-cli=bls-runtime --workspace=/data/workspace \
+          --private-key=/data/keys/priv.bin --log-level=debug --port=9021 \
+          --boot-nodes=/ip4/172.22.0.100/tcp/9020/p2p/$HEAD_ID \
+          --topic=allora-topic-2-worker \
+          --allora-chain-key-name=testkey \
+          --allora-chain-restore-mnemonic='$WALLET_SEED_PHRASE' \
+          --allora-node-rpc-address=https://allora-rpc.edgenet.allora.network/ \
+          --allora-chain-topic-id=2
+    volumes:
+      - ./worker-data:/data
+    working_dir: /data
+    depends_on:
+      - inference
+      - head
+    networks:
+      eth-model-local:
+        aliases:
+          - worker
+        ipv4_address: 172.22.0.10
+    restart: always
+
+  head:
+    container_name: head-basic-eth-pred-2
+    image: alloranetwork/allora-inference-base-head:latest
+    environment:
+      - HOME=/data
+    entrypoint:
+      - "/bin/bash"
+      - "-c"
+      - |
+        if [ ! -f /data/keys/priv.bin ]; then
+          echo "Generating new private keys..."
+          mkdir -p /data/keys
+          cd /data/keys
+          allora-keys
+        fi
+        allora-node --role=head --peer-db=/data/peerdb --function-db=/data/function-db  \
+          --runtime-path=/app/runtime --runtime-cli=bls-runtime --workspace=/data/workspace \
+          --private-key=/data/keys/priv.bin --log-level=debug --port=9020 --rest-api=:6010
+    ports:
+      - "6010:6010"
+    volumes:
+      - ./head-data:/data
+    working_dir: /data
+    networks:
+      eth-model-local:
+        aliases:
+          - head
+        ipv4_address: 172.22.0.100
+    restart: always
+
+networks:
+  eth-model-local:
+    driver: bridge
+    ipam:
+      config:
+        - subnet: 172.22.0.0/24
+
+volumes:
+  inference-data:
+  worker-data:
+  head-data:
+EOF
+
+echo "docker-compose.yml file generated successfully!"
+echo
+
+echo "Building and starting Docker containers..."
+docker-compose build
+docker-compose up -d
+echo
+
+echo "Checking running Docker containers..."
+docker ps
