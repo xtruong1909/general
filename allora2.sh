420f4ba7ee12536b00f6b76a085b3cf01ae0c9bf
hiepntnaa <160826386+hiepntnaa@users.noreply.github.com>
2024-07-16 13:47:48 +0700

    Update allora2.sh

--- allora2.sh
+++ allora2.sh
@@ -12,16 +12,9 @@ execute_with_prompt() {
 cd allora-chain/
 git clone -b worker-face-10m https://github.com/nhunamit/basic-coin-prediction-node worker-face-10m
 cd worker-face-10m/
-mkdir worker-data
-mkdir head-data
-sudo chmod -R 777 worker-data head-data
-echo
-echo "Creating Head keys..."
-echo
-sudo docker run -it --entrypoint=bash -v $(pwd)/head-data:/data alloranetwork/allora-inference-base:latest -c "mkdir -p /data/keys && (cd /data/keys && allora-keys)"
-echo
-sudo docker run -it --entrypoint=bash -v $(pwd)/worker-data:/data alloranetwork/allora-inference-base:latest -c "mkdir -p /data/keys && (cd /data/keys && allora-keys)"
-echo
+mkdir -p worker-topic-2-data
+chmod 777 worker-topic-2-data
+
 
 echo "This is your Head ID:"
 HEAD_ID=$(cat head-data/keys/identity)
@@ -58,7 +51,7 @@ services:
       # test: ["CMD", "curl", "-f", "http://localhost:8011/inference/ETH"]
       test: ["CMD-SHELL", "curl -f http://localhost:8011/inference/ETH || exit 1 && curl -f http://localhost:8011/inference/BTC || exit 1 && curl -f http://localhost:8011/inference/SOL || exit 1"]
       interval: 10s
-      timeout: 5s
+      timeout: 20s
       retries: 12
       start_period: 600s
     volumes:
@@ -106,7 +99,7 @@ services:
         allora-node --role=worker --peer-db=/data/peerdb --function-db=/data/function-db \
           --runtime-path=/app/runtime --runtime-cli=bls-runtime --workspace=/data/workspace \
           --private-key=/data/keys/priv.bin --log-level=debug --port=9102 \
-          --boot-nodes=/dns4/head-0-p2p.edgenet.allora.network/tcp/32050/p2p/12D3KooWQgcJ4wiHBWE6H9FxZAVUn84ZAmywQdRk83op2EibWTiZ,/dns4/head-0-p2p.edgenet.allora.network/tcp/32080/p2p/12D3KooWQgcJ4wiHBWE6H9FxZAVUn84ZAmywQdRk83op2EibWTiZ,/dns4/head-1-p2p.edgenet.allora.network/tcp/32081/p2p/12D3KooWCyao1YJ9DDZEAV8ZUZ1MLLKbcuxVNju1QkTVpanN9iku,/dns4/head-2-p2p.edgenet.allora.network/tcp/32082/p2p/12D3KooWKZYNUWBjnAvun6yc7EBnPvesX23e5F4HGkEk1p5Q7JfK \
+          --boot-nodes=/dns4/head-0-p2p.edgenet.allora.network/tcp/32067/p2p/12D3KooWNiJ9KPtBwCSzbuvXL544U9FtWwvbLUH2VJcv1HyFMxj1,/dns4/head-0-p2p.edgenet.allora.network/tcp/32067/p2p/12D3KooWNiJ9KPtBwCSzbuvXL544U9FtWwvbLUH2VJcv1HyFMxj1,/dns4/head-1-p2p.edgenet.allora.network/tcp/32061/p2p/12D3KooWMbXijKMcsJfeBGkPavq5Q3Mh7m83DUvd8CHVDUKbpXDa,/dns4/head-2-p2p.edgenet.allora.network/tcp/32069/p2p/12D3KooWN3zVFkEDDH1DGJGHmJDZnB6ghhhKDz4Vzat6Z6P5i9y5 \
           --topic=allora-topic-2-worker \
           --allora-chain-key-name=net1_worker \
           --allora-chain-restore-mnemonic='$WALLET_SEED_PHRASE' \
@@ -126,87 +119,6 @@ services:
           - worker_topic_2
         ipv4_address: 172.32.0.21
 
-  worker_topic_4:
-    container_name: net1-worker2-topic-4
-    environment:
-      - INFERENCE_API_ADDRESS=http://inference:8011
-      - HOME=/data
-    build:
-      context: .
-      dockerfile: Dockerfile_b7s
-    entrypoint:
-      - "/bin/bash"
-      - "-c"
-      - |
-        if [ ! -f /data/keys/priv.bin ]; then
-          echo "Generating new private keys..."
-          mkdir -p /data/keys
-          cd /data/keys
-          allora-keys
-        fi
-        allora-node --role=worker --peer-db=/data/peerdb --function-db=/data/function-db \
-          --runtime-path=/app/runtime --runtime-cli=bls-runtime --workspace=/data/workspace \
-          --private-key=/data/keys/priv.bin --log-level=debug --port=9104 \
-          --boot-nodes=/dns4/head-0-p2p.edgenet.allora.network/tcp/32050/p2p/12D3KooWQgcJ4wiHBWE6H9FxZAVUn84ZAmywQdRk83op2EibWTiZ,/dns4/head-0-p2p.edgenet.allora.network/tcp/32080/p2p/12D3KooWQgcJ4wiHBWE6H9FxZAVUn84ZAmywQdRk83op2EibWTiZ,/dns4/head-1-p2p.edgenet.allora.network/tcp/32081/p2p/12D3KooWCyao1YJ9DDZEAV8ZUZ1MLLKbcuxVNju1QkTVpanN9iku,/dns4/head-2-p2p.edgenet.allora.network/tcp/32082/p2p/12D3KooWKZYNUWBjnAvun6yc7EBnPvesX23e5F4HGkEk1p5Q7JfK \
-          --topic=allora-topic-4-worker \
-          --allora-chain-key-name=net1_worker \
-          --allora-chain-restore-mnemonic='$WALLET_SEED_PHRASE' \
-          --allora-chain-topic-id=4 \
-          --allora-node-rpc-address=https://allora-rpc.edgenet.allora.network \
-          --allora-chain-worker-mode=worker
-    volumes:
-      - ./worker-topic-4-data:/data
-    working_dir: /data
-    depends_on:
-      - inference
-    ports:
-      - "9104:9104"
-    networks:
-      net1-worker2:
-        aliases:
-          - worker_topic_4
-        ipv4_address: 172.32.0.23
-
-  worker_topic_6:
-    container_name: net1-worker2-topic-6
-    environment:
-      - INFERENCE_API_ADDRESS=http://inference:8011
-      - HOME=/data
-    build:
-      context: .
-      dockerfile: Dockerfile_b7s
-    entrypoint:
-      - "/bin/bash"
-      - "-c"
-      - |
-        if [ ! -f /data/keys/priv.bin ]; then
-          echo "Generating new private keys..."
-          mkdir -p /data/keys
-          cd /data/keys
-          allora-keys
-        fi
-        allora-node --role=worker --peer-db=/data/peerdb --function-db=/data/function-db \
-          --runtime-path=/app/runtime --runtime-cli=bls-runtime --workspace=/data/workspace \
-          --private-key=/data/keys/priv.bin --log-level=debug --port=9106 \
-          --boot-nodes=/dns4/head-0-p2p.edgenet.allora.network/tcp/32050/p2p/12D3KooWQgcJ4wiHBWE6H9FxZAVUn84ZAmywQdRk83op2EibWTiZ,/dns4/head-0-p2p.edgenet.allora.network/tcp/32080/p2p/12D3KooWQgcJ4wiHBWE6H9FxZAVUn84ZAmywQdRk83op2EibWTiZ,/dns4/head-1-p2p.edgenet.allora.network/tcp/32081/p2p/12D3KooWCyao1YJ9DDZEAV8ZUZ1MLLKbcuxVNju1QkTVpanN9iku,/dns4/head-2-p2p.edgenet.allora.network/tcp/32082/p2p/12D3KooWKZYNUWBjnAvun6yc7EBnPvesX23e5F4HGkEk1p5Q7JfK \
-          --topic=allora-topic-6-worker \
-          --allora-chain-key-name=net1_worker \
-          --allora-chain-restore-mnemonic='$WALLET_SEED_PHRASE' \
-          --allora-chain-topic-id=6 \
-          --allora-node-rpc-address=https://allora-rpc.edgenet.allora.network \
-          --allora-chain-worker-mode=worker
-    volumes:
-      - ./worker-topic-6-data:/data
-    working_dir: /data
-    depends_on:
-      - inference
-    ports:
-      - "9106:9106"
-    networks:
-      net1-worker2:
-        aliases:
-          - worker_topic_6
-        ipv4_address: 172.32.0.25
   
 networks:
   net1-worker2:
