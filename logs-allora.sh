1504bbf3a598b4f7f959d04188bcc9b0ccbd8891
hiepntnaa <160826386+hiepntnaa@users.noreply.github.com>
2024-07-18 11:31:21 +0700

    Update logs-allora.sh

--- logs-allora.sh
+++ logs-allora.sh
@@ -17,7 +17,7 @@ echo -e "${YELLOW}Nubit Node${NC}"
 journalctl -u nubit -n 6 -o cat --no-pager
 
 # Gửi request bằng curl và lưu output vào biến
-output=$(
+output1=$(
 curl -s --location 'http://localhost:6000/api/v1/functions/execute' \
 --header 'Content-Type: application/json' \
 --data '{
@@ -42,9 +42,66 @@ curl -s --location 'http://localhost:6000/api/v1/functions/execute' \
 }')
 
 echo
-# Hiển thị "Allora Worker Node" màu xanh lá cây
-echo -e "${GREEN}Allora Worker Node:${NC}"
-echo "$output"
+echo -e "${GREEN}Allora Worker-1:${NC}"
+echo "$output1"
+echo
+
+output2=$(
+curl -s --location 'http://localhost:6000/api/v1/functions/execute' \
+--header 'Content-Type: application/json' \
+--data '{
+    "function_id": "bafybeigpiwl3o73zvvl6dxdqu7zqcub5mhg65jiky2xqb4rdhfmikswzqm",
+    "method": "allora-inference-function.wasm",
+    "parameters": null,
+    "topic": "2",
+    "config": {
+        "env_vars": [
+            {
+                "name": "BLS_REQUEST_PATH",
+                "value": "/api"
+            },
+            {
+                "name": "ALLORA_ARG_PARAMS",
+                "value": "ETH"
+            }
+        ],
+        "number_of_nodes": -1,
+        "timeout": 2
+    }
+}')
+
+echo
+echo -e "${GREEN}Allora Worker-1:${NC}"
+echo "$output2"
+echo
+
+output7=$(
+curl -s --location 'http://localhost:6000/api/v1/functions/execute' \
+--header 'Content-Type: application/json' \
+--data '{
+    "function_id": "bafybeigpiwl3o73zvvl6dxdqu7zqcub5mhg65jiky2xqb4rdhfmikswzqm",
+    "method": "allora-inference-function.wasm",
+    "parameters": null,
+    "topic": "7",
+    "config": {
+        "env_vars": [
+            {
+                "name": "BLS_REQUEST_PATH",
+                "value": "/api"
+            },
+            {
+                "name": "ALLORA_ARG_PARAMS",
+                "value": "ETH"
+            }
+        ],
+        "number_of_nodes": -1,
+        "timeout": 2
+    }
+}')
+
+echo
+echo -e "${GREEN}Allora Worker-1:${NC}"
+echo "$output7"
 echo
 
 exit 0
