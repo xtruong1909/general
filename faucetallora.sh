69e721692be99e1385efefa1428a9affb0e498bc
hiepntnaa <160826386+hiepntnaa@users.noreply.github.com>
2024-07-18 21:50:30 +0700

    Update faucetallora.sh

--- faucetallora.sh
+++ faucetallora.sh
@@ -6,9 +6,9 @@ address=$(yes $pass | allorad keys list | grep address | awk '{print $3}')
 echo $address
 
 
-for ((i=1; i<=30; i++)); do
+for ((i=1; i<=10; i++)); do
   echo "faucet n $i..."
-  curl -sS "https://faucet.testnet-1.testnet.allora.network/send/testnet-1/$address"
+  curl -sS "https://faucet.testnet-1.testnet.allora.network/send/allora-testnet-1/$address"
   sleep 10
 done
 
