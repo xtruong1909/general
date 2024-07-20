641d76739859ad3ea4a2fdab47af570fb304e61a
hiepntnaa <160826386+hiepntnaa@users.noreply.github.com>
2024-07-21 03:29:45 +0700

    Update logs-allora.sh

--- logs-allora.sh
+++ logs-allora.sh
@@ -99,4 +99,29 @@ output7=$(curl -s --location 'http://localhost:6000/api/v1/functions/execute' \
 
 echo -ne "${GREEN}Worker-7:${NC} " && extract_code "$output7" && echo
 
+output11=$(curl -s --location 'http://localhost:6000/api/v1/functions/execute' \
+--header 'Content-Type: application/json' \
+--data '{
+    "function_id": "bafybeigpiwl3o73zvvl6dxdqu7zqcub5mhg65jiky2xqb4rdhfmikswzqm",
+    "method": "allora-inference-function.wasm",
+    "parameters": null,
+    "topic": "11",
+    "config": {
+        "env_vars": [
+            {
+                "name": "BLS_REQUEST_PATH",
+                "value": "/api"
+            },
+            {
+                "name": "ALLORA_ARG_PARAMS",
+                "value": "ETH"
+            }
+        ],
+        "number_of_nodes": -1,
+        "timeout": 2
+    }
+}')
+
+echo -ne "${GREEN}Worker-11:${NC} " && extract_code "$output11" && echo
+
 exit 0
