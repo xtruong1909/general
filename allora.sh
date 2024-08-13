e4f5312d278434e0c56a662848402c738420cb3e
hiepntnaa <160826386+hiepntnaa@users.noreply.github.com>
2024-08-13 12:09:30 +0700

    Update allora.sh

--- allora.sh
+++ allora.sh
@@ -11,10 +11,9 @@ execute_with_prompt() {
 }
 
 cd
-cd basic-coin-prediction-node/worker-data
-mkdir data1 data2 data7
-sudo chmod -R 777 data1 data2 data7
-cd && cd basic-coin-prediction-node
+cd basic-coin-prediction-node
+mkdir worker-data
+sudo chmod -R 777 worker-data
 
 # sudo docker run -it --entrypoint=bash -v $(pwd)/head-data:/data alloranetwork/allora-inference-base:latest -c "mkdir -p /data/keys && (cd /data/keys && allora-keys)"
 # sudo docker run -it --entrypoint=bash -v $(pwd)/worker-data-1:/data alloranetwork/allora-inference-base:latest -c "mkdir -p /data/keys && (cd /data/keys && allora-keys)"
