2a80ac6226f71fe9238b0971bdb5c8367cdfdcc8
hiepntnaa <160826386+hiepntnaa@users.noreply.github.com>
2024-09-01 16:56:27 +0700

    Create elixir-node.sh

---
+++ elixir-node.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+mkdir -p /elixir
+
+touch /elixir/validator.env
+
+echo "This is your IP:"
+IP=$(curl ifconfig.me)
+
+read -p "$(printf '\033[1;92m')Nhap ten node: $(printf '\033[0m')" ELIXIR_NODE_NAME
+read -p "$(printf '\033[1;92m')Nhap dia chi vi nhan airdrop: $(printf '\033[0m')" ADDR_WALLET
+read -p "$(printf '\033[1;92m')Nhap private key: $(printf '\033[0m')" PRIV_KEY
+
+echo "Da luu thong tin vao /elixir/validator.env file..."
+
+cat <<EOF > /elixir/validator.env
+ENV=testnet-3
+
+STRATEGY_EXECUTOR_IP_ADDRESS=$IP
+STRATEGY_EXECUTOR_DISPLAY_NAME=$ELIXIR_NODE_NAME
+STRATEGY_EXECUTOR_BENEFICIARY=$ADDR_WALLET
+SIGNER_PRIVATE_KEY=$PRIV_KEY
+
+EOF
+
+docker pull elixirprotocol/validator:v3
+
+if [ $(docker ps -a -q -f name=elixir) ]; then
+    docker stop elixir
+    docker rm elixir
+fi
+
+docker run -d --env-file /elixir/validator.env --name elixir --restart unless-stopped elixirprotocol/validator:v3
