b107aa035bd0552a9df1cd5a0c112d4fb407e00d
hiepntnaa <160826386+hiepntnaa@users.noreply.github.com>
2024-10-06 11:30:59 +0700

    Update .checkswap.sh

--- .checkswap.sh
+++ .checkswap.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# Check swap used > 1.5GB
+# Check swap used > 2GB
 USED_SWAP=$(free | grep Swap | awk '{print $3}')
-if [ "$USED_SWAP" -gt 1572864 ]; then
-  echo "Swap usage is greater than 1.5GB. Proceeding with service restart..."
+if [ "$USED_SWAP" -gt 2097152 ]; then
+  echo "Swap usage is greater than 2GB. Proceeding with service restart..."
   
   # Stop dill and hemi
   systemctl stop dill
-  systemctl stop hemi
   
   # Wait 30s
   sleep 30
   
   # Restart dill and hemi
   systemctl restart dill
-  systemctl restart hemi
   
   echo "Services dill and hemi have been restarted."
 else
-  echo "Swap usage is below 1.5GB. No action taken."
+  echo "Swap usage is below 2GB. No action taken."
 fi
