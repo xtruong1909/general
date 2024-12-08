e2982147e8e458fffc3e8684ee656821fbfca3e1
hiepntnaa <160826386+hiepntnaa@users.noreply.github.com>
2024-12-08 13:14:17 +0700

    Create .restart-chrome.sh

---
+++ .restart-chrome.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo -u $(logname) bash << 'INNER_EOF'
+export DISPLAY=:1100
+pkill chrome && pkill grass && sleep 10
+google-chrome --remote-debugging-port=9222 https://app.gradient.network/dashboard/node about:blank &
+sleep 30
+
+DEBUG_URL="http://localhost:9222/json"
+
+for i in {1..3}; do
+  TAB_INFO=$(curl -s $DEBUG_URL)
+  TAB_ID=$(echo $TAB_INFO | jq -r '.[] | select(.title == "Gradient Network Dashboard") | .id')
+  if [ -n "$TAB_ID" ]; then
+    curl -X DELETE "$DEBUG_URL/close/$TAB_ID"
+    echo "Tab with title 'Gradient Network Dashboard' closed."
+    break
+  else
+    echo "No tab with title 'Gradient Network Dashboard' found. Attempt $i of 3."
+    sleep 45
+  fi
+done
+sleep 15
+grass &
+INNER_EOF
